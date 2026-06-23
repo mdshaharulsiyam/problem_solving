024a52f87ffed36a23ae022f3dc827925b95fe9a
shaharulsiyam <shaharulsiyam0273@gmail.com>
2026-06-23 15:19:02 +0600

    update

---
+++ codechef/Akash_and_Function.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+
+    while (T--) {
+        long long N;
+        cin >> N;
+        cout << (N + 1) / 2 << "\n";
+    }
+
+    return 0;
+}
